22f5de273227ca68805accbf774885f58a1b95f4
NODIRA <mir-n-mir@mail.ru>
2023-07-27 12:55:27 +0300

    WPF

---
+++ MyWpfApp/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyWpfApp
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow mainWindow = new MainWindow(); // Создание экземпляра главного окна
+            mainWindow.Show(); // Отображение главного окна
+        }
+    }
+}
